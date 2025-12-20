42694bd26d90e68b0902eadf5b517decd070f742
dio067 <yousefnas06@gmail.com>
2025-12-20 20:12:41 +0400

    feat: add db schema and migrations

---
+++ server/prisma/migrations/20251220160753_init/migration.sql
@@ -0,0 +1,49 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "event" TEXT,
+    "site" TEXT,
+    "white" TEXT,
+    "black" TEXT,
+    "result" TEXT,
+    "playedAt" TIMESTAMP(3),
+    "pgn" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Move" (
+    "id" TEXT NOT NULL,
+    "gameId" TEXT NOT NULL,
+    "ply" INTEGER NOT NULL,
+    "san" TEXT NOT NULL,
+    "uci" TEXT,
+
+    CONSTRAINT "Move_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Game" ADD CONSTRAINT "Game_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Move" ADD CONSTRAINT "Move_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
